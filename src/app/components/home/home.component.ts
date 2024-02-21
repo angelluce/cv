a2f52dc4a31d9977b41e4135a95a989d37d43a0c
angelluce <angelluce244@gmail.com>
2024-02-20 21:47:14 -0500

    ViewportScroller injected

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Informacion} from "../../models/Informacion";
 import {InformacionService} from "../../services/informacion.service";
 import {environment} from "../../../environments/environment";
+import {ViewportScroller} from "@angular/common";
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,16 @@ import {environment} from "../../../environments/environment";
 export class HomeComponent implements OnInit {
   informacion: Informacion;
 
-  constructor(private informacionService: InformacionService) {
+  constructor(private informacionService: InformacionService,
+              private viewportScroller: ViewportScroller) {
     this.informacion = informacionService.obtenerInformacionCompleta();
   }
 
   ngOnInit(): void {
+  }
+
+  public onClickScrollToElement(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
   }
 
   protected readonly environment = environment;
